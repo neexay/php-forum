744dbcb95dd1e2701052403aa1dd6758fb78ce77
Victor Kaspersky <69204774+php1995@users.noreply.github.com>
2020-12-19 23:59:13 +0300

    Delete config.php

--- localhost/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "";
-# $db_name = $db_host;
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name); // коннект с сервером бд
-$mysqli->set_charset("utf8mb4"); // задаем кодировку
