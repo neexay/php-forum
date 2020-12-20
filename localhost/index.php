ad336ab6aa491b291b32d48f3415e52601eeee89
rillit00 <60571909+rillit00@users.noreply.github.com>
2020-12-20 22:08:12 +0700

    Add new index.php

---
+++ localhost/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Php форум</title>
+    <link rel="stylesheet" href="css/header_and_footer.css" type="text/css">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <link rel="stylesheet" href="css/content.css" type="text/css">
+    <meta name="description" content="PHP петушатник">
+    <meta name="keywords" content="PHP, Backend">
+    <meta name="viewport" content="width=device-width initial-scale=1">
+    <link href="img/php.ico" rel="shortcut icon" type="image/x-icon">
+</head>
+<body>
+<?php require_once 'pages/header.html'?>
+<?php require_once 'pages/content.html' ?>
+<?php require_once 'pages/footer.html' ?>
+</body>
+</html>
+
