6f029f71e0c9c88a784109f0b536e62997a234cd
Kewin <test@test.te>
2020-12-21 12:32:12 +0200

    changed project structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-форум</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="menu">
+        <ul class="menu__list">
+            <li>
+                <a href="#" class="menu__link">Главная</a>
+            </li>
+            <li>
+                <a href="#" class="menu__link">Javascript</a>
+            </li>
+            <li>
+                <a href="#" class="menu__link">HTML&CSS</a>
+            </li>
+            <li>
+                <a href="#" class="menu__link">PHP</a>
+            </li>
+            <li>
+                <a href="#" class="menu__link">SQL</a>
+            </li>
+        </ul>
+    </nav> 
+    
+</body>
+</html>
