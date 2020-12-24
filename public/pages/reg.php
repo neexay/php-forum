3c7385851da48ea11531dc269b8cc78de1c81db6
rillit00 <60571909+rillit00@users.noreply.github.com>
2020-12-24 20:43:42 +0700

    Настроены страницы с регистрацией/авторизацией
    Но страница регистрация почему-то еще не показывается.

---
+++ public/pages/reg.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/css/menucontent.css">
+    <title>Регистрация</title>
+</head>
+<body>
+<div class="pagewrapper">
+                <div class="a1">
+                    <?php require_once("public/include_pages/header.html");?>
+                </div>
+                <div class="sidebar">
+                    <?php require_once("public/include_pages/sidebar.html");?>
+                </div>
+            <div class="sidecont">
+                <div class="confoot">
+                    <div class="con">
+                        <?php require_once("public/include_pages/reg.html");?>
+                    </div>
+                    <div class="foo">
+                        <?php require_once("public/include_pages/footer.html");?>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
