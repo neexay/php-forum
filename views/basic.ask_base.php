ca519dde21c0fecb0c97e20ac3abffe8f4b88e03
Victor Kaspersky <69204774+php1995@users.noreply.github.com>
2020-12-25 20:31:48 +0300

    Update and rename basic.ask.php to basic.ask_base.php

--- views/basic.ask_base.php
+++ views/basic.ask_base.php
@@ -9,7 +9,7 @@
         <div class="title">
             <h3>Суть вопроса</h3>
             <p>Сформулируйте вопрос так, чтобы было сразу понятно, о чем речь</p>
-            <!-------обработка поля в php-->
+            <!--обработка поля в php-->
             <textarea name="question-title" id="question" cols="130" rows="2" maxlength="130"
                       placeholder="Введите вопрос"
                       required></textarea>
@@ -18,7 +18,7 @@
         <div class="title">
             <h3>Теги вопроса</h3>
             <p>Ключевые слова от 1 до 5 штук</p>
-            <!-------обработка поля в php-->
+            <!--обработка поля в php-->
             <textarea name="question-tags" id="tags" cols="130" rows="2" maxlength="130"
                       placeholder="Введите теги"
                       required></textarea>
@@ -28,8 +28,8 @@
         <div class="title">
             <h3>Детали вопроса</h3>
             <p>Опишите в подробностях свой вопрос</p>
-            <!------обработка поля в php-->
-            <!---------!!!!написать текстовой редактор(js/php)-->
+            <!--обработка поля в php-->
+            <!--!!!!написать текстовой редактор(js/php)-->
             <textarea name="question-desc" id="question_description" cols="130" rows="10" maxlength="600"
                       placeholder="Введите подробности" required></textarea>
         </div>
