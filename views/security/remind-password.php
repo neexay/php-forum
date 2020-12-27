c97728e7edd18ea3bbb68acb830b6576aa053387
rillit00 <60571909+rillit00@users.noreply.github.com>
2020-12-28 00:40:48 +0700

    Update remind-password.php

--- views/security/remind-password.php
+++ views/security/remind-password.php
@@ -1,7 +1,3 @@
-<!--Задачи для фронтенда:
-1)инпут "введите логин или почту"
-2)кнопка "Я вспомнил пароль" под этим инпутом, href="/security/login"
-
 
 Задачи бекендам:
 1)если введена почта, проверить наличие такой почты в бд:
@@ -10,3 +6,38 @@
 
 2)если введен логин - найти в базе данных почту этого логина и скинуть пароль на почту
 -->
+<!-- Установка заголовка страницы ДЛЯ ТЕКУЩЕЙ СТРАНИЦЫ! -->
+<?php if(!isset($title)){ ob_start(); ?>
+    Форум-главная страница
+    <?php $title = ob_get_clean();} ?>
+<!-- Установка СОДЕРЖИМОГО страницы ДЛЯ ТЕКУЩЕЙ СТРАНИЦЫ! -->
+<?php if(!isset($content)){ ob_start(); ?>
+    <form class="formWithValidation" id="remind_form">
+                <div class="form">
+                    <div class="title">
+                        <h2>Восстановление пароля</h2>
+                        <hr>
+                    </div>
+
+                    <div class="title"> 
+                        <h3>Введите логин или почту</h3>        
+                        <input type="text/email" size="30" required>
+                        <p>Вам придет письмо с дальнейшими инструкциями</p>
+                    </div>
+                    
+                    <input type="submit" class="btn" value="Выслать письмо">
+
+                    <div class="title">
+                      <p>Я вспомнил(а) пароль. <a href="/security/login">Войти</a> </p>
+                    </div>
+
+                </div>    
+
+    </form>
+     
+
+    <?php $content = ob_get_clean();} ?>
+
+
+<!-- ЗДЕСЬ ВСЕ НАСЛЕДУЕТСЯ ИЗ ШАБЛОНА! -->
+<?php require 'views/include/pages_template.php'; ?>
