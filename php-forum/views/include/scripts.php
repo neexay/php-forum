c0245c70c2bd01c647892ae4478f3bcbf710a9c3
neexay <woio@bk.ru>
2021-01-04 17:18:16 +0300

    обнова

--- php-forum/views/include/scripts.php
+++
@@ -1,4 +0,0 @@
-<script src="/php-forum/publicrum/public/js/logout.js"></script>
-<script src="/php-forum/publicrum/public/includes/js/jquery.min.js"></script>
-<script src="/php-forum/publicrum/public/includes/js/sweet-alerts.init.js"></script>
-<script src="/php-forum/publicrum/public/includes/js/sweetalert2.all.min.js"></script>
--- php-forum/views/include/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar">
-    <a href="/">&#10068; Все вопросы</a>
-    <a href="/navigation/tags">&#9758; Все теги</a>
-    <a href="/navigation/feedback">&#9993; Сообщить об ошибке</a>
-    <? if (isset($_SESSION['id'])) { ?>
-        <a href="/security/profile">Мой профиль</a>
-        <a id="logout">выход</a>
-    <? } ?>
-</div>
